cda89dfa52161d917c03c1dc7e92112ef6f060df
Gustavo Alves <alvesvasconcelosg@gmail.com>
2020-11-26 22:39:38 -0300

    insert e delete User

--- src/main/java/com/praticajava/pratica/resources/UserResource.java
+++ src/main/java/com/praticajava/pratica/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.praticajava.pratica.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.praticajava.pratica.entities.User;
 import com.praticajava.pratica.services.UserService;
@@ -22,7 +27,6 @@ public class UserResource {
 	
 	@Autowired
 	private UserService service;
-	
 	
 	
 	//Método end-point para acessar os usuários 
@@ -45,4 +49,34 @@ public class UserResource {
 		
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/praticajava/pratica/services/UserService.java
+++ src/main/java/com/praticajava/pratica/services/UserService.java
@@ -40,4 +40,13 @@ public class UserService {
 		return obj.get();
 		
 	}
+	
+	//Metodo save já retorna o obj
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
